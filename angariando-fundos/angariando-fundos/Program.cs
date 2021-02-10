043350d24b7e3ce0b47f97e290d83f77b96d51af
VictorSamp <victor.msampaio@outlook.com.br>
2021-02-10 18:14:03 -0300

    Adicionando desafio 'Angariando fundos' e alterando desafio 'coordenadas de um ponto'

---
+++ angariando-fundos/angariando-fundos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace angariando_fundos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
