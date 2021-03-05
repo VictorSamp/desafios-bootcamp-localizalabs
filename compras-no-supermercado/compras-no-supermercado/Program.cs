bda0815d7bb2e2df7761c0cd0432b0cc7e28b005
VictorSamp <victor.msampaio@outlook.com.br>
2021-03-05 12:42:57 -0300

    Finalizando exercícios 'Compras no Supermercado' e 'Pink e Cérebro'

---
+++ compras-no-supermercado/compras-no-supermercado/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace compras_no_supermercado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeroDeTestes = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numeroDeTestes; i++)
+            {
+                List<string> listDeCompras = new List<String>(Console.ReadLine().Split(' '));
+                List<string> listaSemDuplicados = listDeCompras.Distinct().ToList();
+                listaSemDuplicados.Sort();
+
+                foreach (string item in listaSemDuplicados)
+                {
+                    Console.Write($"{item} ");
+                }
+
+                Console.WriteLine(" ");
+            }
+        }
+    }
+}
---
+++ pink-e-cerebro/pink-e-cerebro/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace pink_e_cerebro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantidadeNumeros = int.Parse(Console.ReadLine());
+
+            string[] listaPink = new string[quantidadeNumeros];
+            listaPink = Console.ReadLine().Split(' ');
+
+            int multiplosDe2 = 0,
+                multiplosDe3 = 0,
+                multiplosDe4 = 0,
+                multiplosDe5 = 0;
+
+            foreach (var numero in listaPink)
+            {
+                int numeroConvertido = int.Parse(numero);
+
+                if (numeroConvertido % 2 == 0) multiplosDe2++;
+                if (numeroConvertido % 3 == 0) multiplosDe3++;
+                if (numeroConvertido % 4 == 0) multiplosDe4++;
+                if (numeroConvertido % 5 == 0) multiplosDe5++;
+            }
+
+            Console.WriteLine($"{multiplosDe2} Multiplo(s) de 2");
+            Console.WriteLine($"{multiplosDe3} Multiplo(s) de 3");
+            Console.WriteLine($"{multiplosDe4} Multiplo(s) de 4");
+            Console.WriteLine($"{multiplosDe5} Multiplo(s) de 5");
+        }
+    }
+}
